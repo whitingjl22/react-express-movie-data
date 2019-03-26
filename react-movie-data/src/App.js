bd78a18d40bfb9d21e9b6c33cdb1787ef52316a4
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-25 19:55:41 -0500

    initial commit

---
+++ react-movie-data/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "./App.css"
+import MovieDataPage from "./container/MovieDataPage"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MovieDataPage />
+      </div>
+    )
+  }
+}
+
+export default App
